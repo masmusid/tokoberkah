c28416fa894eb654abcc411bb9baaf4cea852672
azkiya12 <gakrohlali@gmail.com>
2021-04-22 08:33:30 +0700

    validator user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -12,14 +12,20 @@
         action="{{ route('users.update', [$user->id]) }}" method="POST">
         @csrf
         <input type="hidden" value="PUT" name="_method">
+
         <label for="name">Name</label>
-        <input value="{{ $user->name }}" class="form-control" placeholder="Full Name" type="text" name="name" id="name" />
+        <input value="{{old('name') ? old('name') : $user->name }}" class="form-control {{$errors->first('name') ? "is-invalid" : ""}}" placeholder="Full Name" type="text" name="name" id="name" />
+        <div class="invalid-feedback">
+            {{$errors->first('name')}}
+        </div>
         <br>
+
         <label for="username">Username</label>
-        <input value="{{ $user->username }}" disabled class="form-control" placeholder="username" type="text" name="username" id="username" />
+        <input value="{{$user->username}}" disabled class="form-control" placeholder="username" type="text" name="username" id="username" />>
         <br>
+
         <label for="">Status</label>
-        <br />
+        <br/>
         <input
             {{ $user->status == "ACTIVE" ? "checked" : "" }}
             value="ACTIVE" type="radio" class="form-control" id="active" name="status">
@@ -29,23 +35,32 @@
             value="INACTIVE" type="radio" class="form-control" id="inactive" name="status">
         <label for="inactive">Inactive</label>
         <br><br>
+
         <label for="">Roles</label>
         <br>
-        <input type="checkbox" {{in_array("ADMIN", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" id="ADMIN" value="ADMIN">
+        <input type="checkbox" {{in_array("ADMIN", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" class="form-control {{$errors->first('roles') ? "is-invalid" : "" }}" id="ADMIN" value="ADMIN">
         <label for="ADMIN">Administrator</label>
-        <input type="checkbox" {{in_array("STAFF", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" id="STAFF" value="STAFF">
+        <input type="checkbox" {{in_array("STAFF", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" class="form-control {{$errors->first('roles') ? "is-invalid" : "" }}" id="STAFF" value="STAFF">
         <label for="STAFF">Staff</label>
-        <input type="checkbox" {{in_array("CUSTOMER", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" id="CUSTOMER" value="CUSTOMER">
+        <input type="checkbox" {{in_array("CUSTOMER", json_decode($user->roles)) ? "checked" : ""}} name="roles[]" class="form-control {{$errors->first('roles') ? "is-invalid" : "" }}" id="CUSTOMER" value="CUSTOMER">
         <label for="CUSTOMER">Customer</label>
-        <br>
-        <br>
+        <div class="invalid-feedback">
+            {{$errors->first('roles')}}
+        </div>
+        <br><br>
+
         <label for="phone">Phone number</label>
         <br>
-        <input type="text" name="phone" class="form-control" value="{{ $user->phone }}">
+        <input type="text" name="phone" class="form-control {{$errors->first('phone') ? "is-invalid" : ""}}" value="{{old('phone') ? old('phone') : $user->phone}}">
+        <div class="invalid-feedback">
+            {{$errors->first('phone')}}
+        </div>
         <br>
+
         <label for="address">Address</label>
-        <textarea name="address" id="address" class="form-control">{{ $user->address }}</textarea>
+        <textarea name="address" id="address" class="form-control {{$errors->first('address') ? "is-invalid" : ""}}">{{old('address') ? old('address') : $user->address}}</textarea>
         <br>
+
         <label for="avatar">Avatar image</label>
         <br>
         Current avatar: <br>
@@ -57,13 +72,15 @@
         @endif
         <br>
         <input id="avatar" name="avatar" type="file" class="form-control">
-        <small class="text-muted">Kosongkan jika tidak ingin mengubah
-            avatar</small>
+        <small class="text-muted">Kosongkan jika tidak ingin mengubah avatar</small>
+
         <hr class="my-3">
+        
         <label for="email">Email</label>
         <input value="{{ $user->email }}" disabled class="form-control" placeholder="user@mail.com" type="text"
             name="email" id="email" />
         <br>
+        
         <input class="btn btn-primary" type="submit" value="Save" />
     </form>
 </div>
